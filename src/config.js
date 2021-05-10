9ca7c471c72eca8e70092c1a649852fc0500e6ff
Fallayn <fallayn@gmail.com>
2021-05-10 10:45:32 +0200

    Reload Foundry if Babele setting changes

--- src/config.js
+++ src/config.js
@@ -15,11 +15,12 @@ export default [
         name: 'enableCompendiumTranslation',
         data: {
             name: 'Kompendiuminhalte übersetzen',
-            hint: 'Übersetzen der Kompendiuminhalte. Benötigt das Babele-Modul.',
+            hint: 'Übersetzen der Kompendiuminhalte. Benötigt das Babele-Modul. (Bei Änderung wird Foundry neu geladen.)',
             scope: 'client',
             type: Boolean,
             config: true,
-            default: true
+            default: true,
+            onChange: () => window.location.reload()
         }
     }
 ];
