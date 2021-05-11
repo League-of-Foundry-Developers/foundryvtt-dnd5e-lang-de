0e6f899d4adc0c43933381b5fff4e5d5bc3f5467
Fallayn <fallayn@gmail.com>
2021-05-11 05:19:54 +0200

    Add auto creature type converter

--- src/converters.js
+++ src/converters.js
@@ -10,6 +10,9 @@ export default function registerConverters(module_id) {
         },
         'rarity': (rarity) => {
             return convertRarity(rarity)
+        },
+        'type': (type) => {
+            return convertType(type)
         }
     });
 }
@@ -52,4 +55,73 @@ var rarity = {
 
 function convertRarity(r) {
     return rarity[r.toLowerCase()] ? rarity[r.toLowerCase()] : r;
+}
+
+var types = {
+    "aberration (shapechanger)": "Aberration (Gestaltwandler)",
+    "aberration": "Aberration",
+    "beast": "Tier",
+    "celestial (titan)": "Himmlisches Wesen (Titan)",
+    "celestial": "Himmlisches Wesen",
+    "construct": "Konstrukt",
+    "dragon": "Drache",
+    "elemental": "Elementar",
+    "fey": "Feenwesen",
+    "fiend (demon)": "Unhold (Dämon)",
+    "fiend (demon, orc)": "Unhold (Dämon, Ork)",
+    "fiend (demon, shapechanger)": "Unhold (Dämon, Gestaltwandler)",
+    "fiend (devil)": "Unhold (Teufel)",
+    "fiend (devil, shapechanger)": "Unhold (Teufel, Gestaltwandler)",
+    "fiend (gnoll)": "Unhold (Gnoll)",
+    "fiend (shapechanger)": "Unhold (Gestaltwandler)",
+    "fiend (yugoloth)": "Unhold (Yugoloth)",
+    "fiend": "Unhold",
+    "giant (cloud giant)": "Riese (Wolkenriese)",
+    "giant (fire giant)": "Riese (Feuerriese)",
+    "giant (frost giant)": "Riese (Frostriese)",
+    "giant (hill giant)": "Riese (Hügelriese)",
+    "giant (stone giant)": "Riese (Steinriese)",
+    "giant (storm giant)": "Riese (Sturmriese)",
+    "giant": "Riese",
+    "humanoid (aarakocra)": "Humanoider (Aarakocra)",
+    "humanoid (any race)": "Humanoider (jedes Volk)",
+    "humanoid (bullywug)": "Humanoider (Bullywug)",
+    "humanoid (dwarf)": "Humanoider (Zwerg)",
+    "humanoid (elf)": "Humanoider (Elf)",
+    "humanoid (firenewt)": "Humanoider (Feuermolch)",
+    "humanoid (gith)": "Humanoider (Gith)",
+    "humanoid (gnoll)": "Humanoider (Gnoll)",
+    "humanoid (gnome)": "Humanoider (Gnom)",
+    "humanoid (goblinoid)": "Humanoider (Goblinoider)",
+    "humanoid (grimlock)": "Humanoider (Grimlock)",
+    "humanoid (grung)": "Humanoider (Grung)",
+    "humanoid (human)": "Humanoider (Mensch)",
+    "humanoid (human, shapechanger)": "Humanoider (Mensch, Gestaltwandler)",
+    "humanoid (kenku)": "Humanoider (Kenku)",
+    "humanoid (kobold)": "Humanoider (Kobold)",
+    "humanoid (kuo-toa)": "Humanoider (Kuo-toa)",
+    "humanoid (lizardfolk)": "Humanoider (Echsenmensch)",
+    "humanoid (merfolk)": "Humanoider (Meervolk)",
+    "humanoid (orc)": "Humanoider (Ork)",
+    "humanoid (quaggoth)": "Humanoider (Quaggoth)",
+    "humanoid (sahuagin)": "Humanoider (Sahuagin)",
+    "humanoid (shapechanger)": "Humanoider (Gestaltwandler)",
+    "humanoid (thri-kreen)": "Humanoider (Thri-kreen)",
+    "humanoid (troglodyte)": "Humanoider (Troglodyt)",
+    "humanoid (xvart)": "Humanoider (Xvart)",
+    "humanoid (yuan-ti)": "Humanoider (Yuan-ti)",
+    "humanoid": "Humanoider",
+    "monstrosity (shapechanger)": "Monstrosität (Gestaltwandler)",
+    "monstrosity (shapechanger, yuan-ti)": "Monstrosität (Gestaltwandler, Yuan-ti)",
+    "monstrosity (titan)": "Monstrosität (Titan)",
+    "monstrosity": "Monstrosität",
+    "ooze": "Schlick",
+    "plant": "Pflanze",
+    "swarm of Tiny beasts": "Schwarm winziger Tier",
+    "undead (shapechanger)": "Untoter (Gestaltwandler)",
+    "undead": "Untoter"
+}
+
+function convertType(t) {
+    return types[t.toLowerCase()] ? types[t.toLowerCase()] : t;
 }
