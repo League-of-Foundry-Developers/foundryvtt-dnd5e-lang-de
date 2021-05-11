97e13147db5217839cd5501f8778b8d1af08c246
Fallayn <fallayn@gmail.com>
2021-05-11 10:49:33 +0200

    Single quotes for consistency

--- src/converters.js
+++ src/converters.js
@@ -33,9 +33,9 @@ export default function registerConverters(id) {
 
 function toTitelCase(str) {
     let upper = true
-    let newStr = ""
+    let newStr = ''
     for (let i = 0, l = str.length; i < l; i++) {
-        if (str[i] == " ") {
+        if (str[i] == ' ') {
             upper = true
             newStr += str[i]
             continue
@@ -73,13 +73,13 @@ function convertAlignment(a) {
 // Rarity
 
 var rarity = {
-	"common": "Gewöhnlich",
-	"uncommon": "Ungewöhnlich",
-	"rare": "Selten",
-	"very rare": "Sehr selten",
-    "legendary": "Legendär",
-    "artifact": "Artefakt",
-    "unique": "Einzigartig"
+	'common': 'Gewöhnlich',
+	'uncommon': 'Ungewöhnlich',
+	'rare': 'Selten',
+	'very rare': 'Sehr selten',
+    'legendary': 'Legendär',
+    'artifact': 'Artefakt',
+    'unique': 'Einzigartig'
 };
 
 function convertRarity(r) {
@@ -87,68 +87,68 @@ function convertRarity(r) {
 }
 
 var types = {
-    "aberration (shapechanger)": "Aberration (Gestaltwandler)",
-    "aberration": "Aberration",
-    "beast": "Tier",
-    "celestial (titan)": "Himmlisches Wesen (Titan)",
-    "celestial": "Himmlisches Wesen",
-    "construct": "Konstrukt",
-    "dragon": "Drache",
-    "elemental": "Elementar",
-    "fey": "Feenwesen",
-    "fiend (demon)": "Unhold (Dämon)",
-    "fiend (demon, orc)": "Unhold (Dämon, Ork)",
-    "fiend (demon, shapechanger)": "Unhold (Dämon, Gestaltwandler)",
-    "fiend (devil)": "Unhold (Teufel)",
-    "fiend (devil, shapechanger)": "Unhold (Teufel, Gestaltwandler)",
-    "fiend (gnoll)": "Unhold (Gnoll)",
-    "fiend (shapechanger)": "Unhold (Gestaltwandler)",
-    "fiend (yugoloth)": "Unhold (Yugoloth)",
-    "fiend": "Unhold",
-    "giant (cloud giant)": "Riese (Wolkenriese)",
-    "giant (fire giant)": "Riese (Feuerriese)",
-    "giant (frost giant)": "Riese (Frostriese)",
-    "giant (hill giant)": "Riese (Hügelriese)",
-    "giant (stone giant)": "Riese (Steinriese)",
-    "giant (storm giant)": "Riese (Sturmriese)",
-    "giant": "Riese",
-    "humanoid (aarakocra)": "Humanoider (Aarakocra)",
-    "humanoid (any race)": "Humanoider (jedes Volk)",
-    "humanoid (bullywug)": "Humanoider (Bullywug)",
-    "humanoid (dwarf)": "Humanoider (Zwerg)",
-    "humanoid (elf)": "Humanoider (Elf)",
-    "humanoid (firenewt)": "Humanoider (Feuermolch)",
-    "humanoid (gith)": "Humanoider (Gith)",
-    "humanoid (gnoll)": "Humanoider (Gnoll)",
-    "humanoid (gnome)": "Humanoider (Gnom)",
-    "humanoid (goblinoid)": "Humanoider (Goblinoider)",
-    "humanoid (grimlock)": "Humanoider (Grimlock)",
-    "humanoid (grung)": "Humanoider (Grung)",
-    "humanoid (human)": "Humanoider (Mensch)",
-    "humanoid (human, shapechanger)": "Humanoider (Mensch, Gestaltwandler)",
-    "humanoid (kenku)": "Humanoider (Kenku)",
-    "humanoid (kobold)": "Humanoider (Kobold)",
-    "humanoid (kuo-toa)": "Humanoider (Kuo-toa)",
-    "humanoid (lizardfolk)": "Humanoider (Echsenmensch)",
-    "humanoid (merfolk)": "Humanoider (Meervolk)",
-    "humanoid (orc)": "Humanoider (Ork)",
-    "humanoid (quaggoth)": "Humanoider (Quaggoth)",
-    "humanoid (sahuagin)": "Humanoider (Sahuagin)",
-    "humanoid (shapechanger)": "Humanoider (Gestaltwandler)",
-    "humanoid (thri-kreen)": "Humanoider (Thri-kreen)",
-    "humanoid (troglodyte)": "Humanoider (Troglodyt)",
-    "humanoid (xvart)": "Humanoider (Xvart)",
-    "humanoid (yuan-ti)": "Humanoider (Yuan-ti)",
-    "humanoid": "Humanoider",
-    "monstrosity (shapechanger)": "Monstrosität (Gestaltwandler)",
-    "monstrosity (shapechanger, yuan-ti)": "Monstrosität (Gestaltwandler, Yuan-ti)",
-    "monstrosity (titan)": "Monstrosität (Titan)",
-    "monstrosity": "Monstrosität",
-    "ooze": "Schlick",
-    "plant": "Pflanze",
-    "swarm of Tiny beasts": "Schwarm winziger Tier",
-    "undead (shapechanger)": "Untoter (Gestaltwandler)",
-    "undead": "Untoter"
+    'aberration (shapechanger)': 'Aberration (Gestaltwandler)',
+    'aberration': 'Aberration',
+    'beast': 'Tier',
+    'celestial (titan)': 'Himmlisches Wesen (Titan)',
+    'celestial': 'Himmlisches Wesen',
+    'construct': 'Konstrukt',
+    'dragon': 'Drache',
+    'elemental': 'Elementar',
+    'fey': 'Feenwesen',
+    'fiend (demon)': 'Unhold (Dämon)',
+    'fiend (demon, orc)': 'Unhold (Dämon, Ork)',
+    'fiend (demon, shapechanger)': 'Unhold (Dämon, Gestaltwandler)',
+    'fiend (devil)': 'Unhold (Teufel)',
+    'fiend (devil, shapechanger)': 'Unhold (Teufel, Gestaltwandler)',
+    'fiend (gnoll)': 'Unhold (Gnoll)',
+    'fiend (shapechanger)': 'Unhold (Gestaltwandler)',
+    'fiend (yugoloth)': 'Unhold (Yugoloth)',
+    'fiend': 'Unhold',
+    'giant (cloud giant)': 'Riese (Wolkenriese)',
+    'giant (fire giant)': 'Riese (Feuerriese)',
+    'giant (frost giant)': 'Riese (Frostriese)',
+    'giant (hill giant)': 'Riese (Hügelriese)',
+    'giant (stone giant)': 'Riese (Steinriese)',
+    'giant (storm giant)': 'Riese (Sturmriese)',
+    'giant': 'Riese',
+    'humanoid (aarakocra)': 'Humanoider (Aarakocra)',
+    'humanoid (any race)': 'Humanoider (jedes Volk)',
+    'humanoid (bullywug)': 'Humanoider (Bullywug)',
+    'humanoid (dwarf)': 'Humanoider (Zwerg)',
+    'humanoid (elf)': 'Humanoider (Elf)',
+    'humanoid (firenewt)': 'Humanoider (Feuermolch)',
+    'humanoid (gith)': 'Humanoider (Gith)',
+    'humanoid (gnoll)': 'Humanoider (Gnoll)',
+    'humanoid (gnome)': 'Humanoider (Gnom)',
+    'humanoid (goblinoid)': 'Humanoider (Goblinoider)',
+    'humanoid (grimlock)': 'Humanoider (Grimlock)',
+    'humanoid (grung)': 'Humanoider (Grung)',
+    'humanoid (human)': 'Humanoider (Mensch)',
+    'humanoid (human, shapechanger)': 'Humanoider (Mensch, Gestaltwandler)',
+    'humanoid (kenku)': 'Humanoider (Kenku)',
+    'humanoid (kobold)': 'Humanoider (Kobold)',
+    'humanoid (kuo-toa)': 'Humanoider (Kuo-toa)',
+    'humanoid (lizardfolk)': 'Humanoider (Echsenmensch)',
+    'humanoid (merfolk)': 'Humanoider (Meervolk)',
+    'humanoid (orc)': 'Humanoider (Ork)',
+    'humanoid (quaggoth)': 'Humanoider (Quaggoth)',
+    'humanoid (sahuagin)': 'Humanoider (Sahuagin)',
+    'humanoid (shapechanger)': 'Humanoider (Gestaltwandler)',
+    'humanoid (thri-kreen)': 'Humanoider (Thri-kreen)',
+    'humanoid (troglodyte)': 'Humanoider (Troglodyt)',
+    'humanoid (xvart)': 'Humanoider (Xvart)',
+    'humanoid (yuan-ti)': 'Humanoider (Yuan-ti)',
+    'humanoid': 'Humanoider',
+    'monstrosity (shapechanger)': 'Monstrosität (Gestaltwandler)',
+    'monstrosity (shapechanger, yuan-ti)': 'Monstrosität (Gestaltwandler, Yuan-ti)',
+    'monstrosity (titan)': 'Monstrosität (Titan)',
+    'monstrosity': 'Monstrosität',
+    'ooze': 'Schlick',
+    'plant': 'Pflanze',
+    'swarm of Tiny beasts': 'Schwarm winziger Tier',
+    'undead (shapechanger)': 'Untoter (Gestaltwandler)',
+    'undead': 'Untoter'
 }
 
 function convertType(t) {
@@ -156,22 +156,22 @@ function convertType(t) {
 }
 
 var races = {
-    "dragonborn": "Drachenblütiger",
-    "dwarf": "Zwerg",
-    "hill dwarf": "Hügelzwerg",
-    "mountain dwarf": "Gebirgszwerg",
-    "elf": "Elf",
-    "high elf": "Hochelf",
-    "gnome": "Gnom",
-    "rock gnome": "Felsengnom",
-    "half elf": "Halbelf",
-    "half-elf": "Halbelf",
-    "halfling": "Halbling",
-    "lightfoot halfling": "Leichtfuss-Halbling",
-    "half orc": "Halbork",
-    "human": "Mensch",
-    "variant human": "Mensch Variante",
-    "tiefling": "Tiefling"
+    'dragonborn': 'Drachenblütiger',
+    'dwarf': 'Zwerg',
+    'hill dwarf': 'Hügelzwerg',
+    'mountain dwarf': 'Gebirgszwerg',
+    'elf': 'Elf',
+    'high elf': 'Hochelf',
+    'gnome': 'Gnom',
+    'rock gnome': 'Felsengnom',
+    'half elf': 'Halbelf',
+    'half-elf': 'Halbelf',
+    'halfling': 'Halbling',
+    'lightfoot halfling': 'Leichtfuss-Halbling',
+    'half orc': 'Halbork',
+    'human': 'Mensch',
+    'variant human': 'Mensch Variante',
+    'tiefling': 'Tiefling'
 }
 
 function convertRace(r) {
@@ -179,89 +179,89 @@ function convertRace(r) {
 }
 
 var languages = {
-    "aarakocra": "Aarakocra",
-    "abyssal": "Abyssisch",
-    "aquan": "Aqual",
-    "auran": "Aural",
-    "celestial": "Celestisch",
-    "common": "Gemeinsprache",
-    "deepspeech": "Tiefensprache",
-    "draconic": "Drakonisch",
-    "druidic": "Druidisch",
-    "druid": "Druidisch",
-    "dwarvish": "Zwergisch",
-    "dwarven": "Zwergisch",
-    "elvish": "Elfisch",
-    "elven": "Elfisch",
-    "giant": "Riesisch",
-    "gith": "Gith",
-    "gnoll": "Gnollisch",
-    "gnomish": "Gnomisch",
-    "goblin": "Goblinisch",
-    "halfling": "Halbingisch",
-    "ignan": "Ignal",
-    "infernal": "Infernalisch",
-    "orc": "Orkisch",
-    "primordial": "Urtümlich",
-    "sylvan": "Sylvanisch",
-    "terran": "Terral",
-    "thievescant": "Diebessprache",
-    "thieves cant": "Diebessprache",
-    "thieves' cant": "Diebessprache",
-    "thieve's cant": "Diebessprache",
-    "undercommon": "Gemeinsprache der Unterreiche",
-    "giant eagle": "Riesenadler",
-    "giant owl": "Rieseneule",
-    "giant elk": "Riesenelch",
-    "worg": "Worg",
-    "winter wolf": "Winterwolf",
-    "sahuagin": "Sahuagin",
-    "common and auran": "Gemeinsprache und Aural",
-    "understands common, elvish, and sylvan but can't speak them": "versteht Gemeinsprache, Elfisch und Sylvanisch, aber kann sie nicht sprechen",
-    "understands infernal but can't speak it": "versteht Infernalisch, kann es aber nicht sprechen",
-    "understands draconic but can't speak": "versteht Drakonisch, kann es aber nicht sprechen",
-    "understands common but doesn't speak it": "versteht Gemeinsprache, kann sie aber nicht sprechen",
-    "understands abyssal but can't speak": "versteht Abbysisch, kann es aber nicht sprechen",
-    "understands all languages it knew in life but can't speak": "versteht alle zur Lebzeiten gesprochenen, kann aber nicht sprechen",
-    "understands commands given in any language but can't speak": "versteht Befehle in allen Sprachen, kann aber nicht sprechen",
-    "(can't speak in rat form)": "(kann in Rattengestalt nicht sprechen)",
-    "(can't speak in boar form)": "(kann in Schweinegestalt nicht sprechen)",
-    "(can't speak in bear form)": "(kann in Bärengestalt nicht sprechen)",
-    "(can't speak in tiger form)": "(kann in Tigergestalt nicht sprechen)",
-    "any one language": "eine nach Wahl",
-    "any two languages": "zwei nach Wahl",
-    "any three languages": "drei nach Wahl",
-    "any four languages": "vier nach Wahl",
-    "any five languages": "fünf nach Wahl",
-    "5 other languages": "5 andere Sprachen",
-    "any, usually common": "beliebig, normalerweise Gemeinsprache",
-    "one language known by its creator": "eine Sprache des Erschaffers",
-    "the languages it knew in life": "jede zu Lebzeiten gesprochene",
-    "those it knew in life": "jede zu Lebzeiten gesprochene",
-    "all it knew in life": "jede zu Lebzeiten gesprochene",
-    "any it knew in life": "jede zu Lebzeiten gesprochene",
-    "all, telepathy 120 ft.": "alle, Telepathie 36m (120ft)",
-    "telepathy 60 ft.": "Telepathie 18m (60ft)",
-    "telepathy 60ft. (works only with creatures that understand abyssal)": "Telepathie 18m (60ft, funktioniert nur mit Kreaturen, die Abyssisch verstehen)",
-    "telepathy 60 ft. (works only with creatures that understand abyssal)": "Telepathie 18m (60ft, funktioniert nur mit Kreaturen, die Abyssisch verstehen)",
-    "telepathy 120 ft.": "Telepathie 36m (120ft)",
-    "but can't speak": "aber kann nicht sprechen",
-    "but can't speak it": "aber kann es nicht sprechen",
-    "choice": "nach Wahl",
-    "understands the languages of its creator but can't speak": "versteht die Sprachen des Erschaffers, aber kann nicht sprechen",
-    "understands common and giant but can't speak": "versteht Gemeinsprache und Riesisch, aber kann nicht sprechen",
-    "cannot speak": "Kann nicht sprechen",
-    "cant speak": "Kann nicht sprechen",
-    "can't speak": "Kann nicht sprechen"
+    'aarakocra': 'Aarakocra',
+    'abyssal': 'Abyssisch',
+    'aquan': 'Aqual',
+    'auran': 'Aural',
+    'celestial': 'Celestisch',
+    'common': 'Gemeinsprache',
+    'deepspeech': 'Tiefensprache',
+    'draconic': 'Drakonisch',
+    'druidic': 'Druidisch',
+    'druid': 'Druidisch',
+    'dwarvish': 'Zwergisch',
+    'dwarven': 'Zwergisch',
+    'elvish': 'Elfisch',
+    'elven': 'Elfisch',
+    'giant': 'Riesisch',
+    'gith': 'Gith',
+    'gnoll': 'Gnollisch',
+    'gnomish': 'Gnomisch',
+    'goblin': 'Goblinisch',
+    'halfling': 'Halbingisch',
+    'ignan': 'Ignal',
+    'infernal': 'Infernalisch',
+    'orc': 'Orkisch',
+    'primordial': 'Urtümlich',
+    'sylvan': 'Sylvanisch',
+    'terran': 'Terral',
+    'thievescant': 'Diebessprache',
+    'thieves cant': 'Diebessprache',
+    'thieves\' cant': 'Diebessprache',
+    'thieve\'s cant': 'Diebessprache',
+    'undercommon': 'Gemeinsprache der Unterreiche',
+    'giant eagle': 'Riesenadler',
+    'giant owl': 'Rieseneule',
+    'giant elk': 'Riesenelch',
+    'worg': 'Worg',
+    'winter wolf': 'Winterwolf',
+    'sahuagin': 'Sahuagin',
+    'common and auran': 'Gemeinsprache und Aural',
+    'understands common, elvish, and sylvan but can\'t speak them': 'versteht Gemeinsprache, Elfisch und Sylvanisch, aber kann sie nicht sprechen',
+    'understands infernal but can\'t speak it': 'versteht Infernalisch, kann es aber nicht sprechen',
+    'understands draconic but can\'t speak': 'versteht Drakonisch, kann es aber nicht sprechen',
+    'understands common but doesn\'t speak it': 'versteht Gemeinsprache, kann sie aber nicht sprechen',
+    'understands abyssal but can\'t speak': 'versteht Abbysisch, kann es aber nicht sprechen',
+    'understands all languages it knew in life but can\'t speak': 'versteht alle zur Lebzeiten gesprochenen, kann aber nicht sprechen',
+    'understands commands given in any language but can\'t speak': 'versteht Befehle in allen Sprachen, kann aber nicht sprechen',
+    '(can\'t speak in rat form)': '(kann in Rattengestalt nicht sprechen)',
+    '(can\'t speak in boar form)': '(kann in Schweinegestalt nicht sprechen)',
+    '(can\'t speak in bear form)': '(kann in Bärengestalt nicht sprechen)',
+    '(can\'t speak in tiger form)': '(kann in Tigergestalt nicht sprechen)',
+    'any one language': 'eine nach Wahl',
+    'any two languages': 'zwei nach Wahl',
+    'any three languages': 'drei nach Wahl',
+    'any four languages': 'vier nach Wahl',
+    'any five languages': 'fünf nach Wahl',
+    '5 other languages': '5 andere Sprachen',
+    'any, usually common': 'beliebig, normalerweise Gemeinsprache',
+    'one language known by its creator': 'eine Sprache des Erschaffers',
+    'the languages it knew in life': 'jede zu Lebzeiten gesprochene',
+    'those it knew in life': 'jede zu Lebzeiten gesprochene',
+    'all it knew in life': 'jede zu Lebzeiten gesprochene',
+    'any it knew in life': 'jede zu Lebzeiten gesprochene',
+    'all, telepathy 120 ft.': 'alle, Telepathie 36m (120ft)',
+    'telepathy 60 ft.': 'Telepathie 18m (60ft)',
+    'telepathy 60ft. (works only with creatures that understand abyssal)': 'Telepathie 18m (60ft, funktioniert nur mit Kreaturen, die Abyssisch verstehen)',
+    'telepathy 60 ft. (works only with creatures that understand abyssal)': 'Telepathie 18m (60ft, funktioniert nur mit Kreaturen, die Abyssisch verstehen)',
+    'telepathy 120 ft.': 'Telepathie 36m (120ft)',
+    'but can\'t speak': 'aber kann nicht sprechen',
+    'but can\'t speak it': 'aber kann es nicht sprechen',
+    'choice': 'nach Wahl',
+    'understands the languages of its creator but can\'t speak': 'versteht die Sprachen des Erschaffers, aber kann nicht sprechen',
+    'understands common and giant but can\'t speak': 'versteht Gemeinsprache und Riesisch, aber kann nicht sprechen',
+    'cannot speak': 'Kann nicht sprechen',
+    'cant speak': 'Kann nicht sprechen',
+    'can\'t speak': 'Kann nicht sprechen'
 }
 
 var languages_suffixes = {
-    " (usually common)": " (normalerweise Gemeinsprache)",
-    " (understands but cannot speak)": " (beherrscht, aber kann nicht sprechen)",
-    " (understands but can't speak)": " (beherrscht, aber kann nicht sprechen)",
-    " but can't speak them": " aber kann sie nicht sprechen",
-    " but cannot speak": " aber kann nicht sprechen",
-    " but can't speak": " aber kann nicht sprechen"
+    ' (usually common)': ' (normalerweise Gemeinsprache)',
+    ' (understands but cannot speak)': ' (beherrscht, aber kann nicht sprechen)',
+    ' (understands but can\'t speak)': ' (beherrscht, aber kann nicht sprechen)',
+    ' but can\'t speak them': ' aber kann sie nicht sprechen',
+    ' but cannot speak': ' aber kann nicht sprechen',
+    ' but can\'t speak': ' aber kann nicht sprechen'
 }
 
 function convertLanguages(l) {
@@ -295,15 +295,15 @@ function convertLanguages(l) {
         }
     })
 
-    return result.join("; ");
+    return result.join('; ');
 }
 
 var monster_name_replacements = {
-    "will-o'-wisp": "Will-o-wisp",
-    "succubus/incubus": "Succubus",
-    "horned devil": "Horned Devil (Malebranche)",
-    "giant rat (diseased)": "Giant Rat",
-    "chain devil": "Chain Devil (Kyton)"
+    'will-o\'-wisp': 'Will-o-wisp',
+    'succubus/incubus': 'Succubus',
+    'horned devil': 'Horned Devil (Malebranche)',
+    'giant rat (diseased)': 'Giant Rat',
+    'chain devil': 'Chain Devil (Kyton)'
 }
 
 function getMonsterID(id) {
@@ -314,11 +314,11 @@ function getMonsterID(id) {
 }
 
 var monster_name_additional = {
-    "succubus/incubus": "Sukkubus/Inkubus",
-    "horned devil": "Hornteufel",
-    "giant rat (diseased)": "Riesenratte (Erkrankt)",
-    "chain devil": "Kettenteufel",
-    "avatar of death": "Avatar des Todes"
+    'succubus/incubus': 'Sukkubus/Inkubus',
+    'horned devil': 'Hornteufel',
+    'giant rat (diseased)': 'Riesenratte (Erkrankt)',
+    'chain devil': 'Kettenteufel',
+    'avatar of death': 'Avatar des Todes'
 }
 
 function convertMonsterName(m, translation, data) {
@@ -332,10 +332,10 @@ function convertMonsterName(m, translation, data) {
 }
 
 var source_book_replacements = {
-    "MM": "MHB",
-    "PHB": "SHB",
-    "DMG": "SLHB",
-    "VGM": "VAM"
+    'MM': 'MHB',
+    'PHB': 'SHB',
+    'DMG': 'SLHB',
+    'VGM': 'VAM'
 }
 
 function convertMonsterSource(m, translation, data) {
@@ -343,9 +343,9 @@ function convertMonsterSource(m, translation, data) {
         return m;
     }
 
-    var new_src = MonsterData.data[data.name].src + " S. " + MonsterData.data[data.name].src_pg;
-    new_src = new_src.replace(", SRD", "");
-    new_src = new_src.replace("SRD", "");
+    var new_src = MonsterData.data[data.name].src + ' S. ' + MonsterData.data[data.name].src_pg;
+    new_src = new_src.replace(', SRD', '');
+    new_src = new_src.replace('SRD', '');
 
     if (game.settings.get(module_id, 'compendiumSrcTranslateBooks')) {
         for (var book in source_book_replacements) {
@@ -354,7 +354,7 @@ function convertMonsterSource(m, translation, data) {
     }
 
     if (game.settings.get(module_id, 'compendiumSrcKeepOriginal')) {
-        new_src = new_src + " (" + m.replace("pg.", "S.") + ")";
+        new_src = new_src + ' (' + m.replace('pg.', 'S.') + ')';
     }
 
     return new_src
