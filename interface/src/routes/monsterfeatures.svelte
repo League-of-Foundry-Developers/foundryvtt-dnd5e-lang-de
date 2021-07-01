811d96e7791c12645f6af4f131a49190b53e97e6
stillday <jan-dennisd@outlook.at>
2021-07-01 10:51:02 +0200

    remove interface. they switch to a other repro

--- interface/src/routes/monsterfeatures.svelte
+++
@@ -1,8 +0,0 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
-<script lang="ts"> 
-	import Translater from '$lib/components/Translater.svelte'; 
-</script>
-<Translater file="dnd5e.monsterfeatures.json"></Translater>
