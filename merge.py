c4debfb1d7b96023f7adcd07d0b8a467acb9e89e
Fallayn <fallayn@gmail.com>
2021-07-31 09:25:00 +0200

    Megre script: add error logging

--- merge.py
+++ merge.py
@@ -12,24 +12,33 @@ path = "languages/"
 
 def main():
     j = None
-    with open(path + "diff.txt", "r", encoding="utf-8") as diff:
-        with open(path + "de.json", "r", encoding="utf-8") as file:
-            j = json.load(file)
+    line = ""
+    linenum = 0
+    
+    try:
+        with open(path + "diff.txt", "r", encoding="utf-8") as diff:
+            with open(path + "de.json", "r", encoding="utf-8") as file:
+                j = json.load(file)
 
-            for line in diff:
-                line = line.strip()
-                if line.endswith(","):
-                  line = line[:-1]
-                line = "{" + line + "}"
+                for line in diff:
+                    linenum += 1
+                    line = line.strip()
+                    if line.endswith(","):
+                      line = line[:-1]
+                    line = "{" + line + "}"
 
-                n = json.loads(line)
-                for k in n.keys():
-                    if k in j and not overwrite:
-                        print("Conflict: key " + str(k) + " exists, ignoring")
-                        continue
+                    n = json.loads(line)
+                    for k in n.keys():
+                        if k in j and not overwrite:
+                            print("Conflict: key " + str(k) + " exists, ignoring")
+                            continue
 
-                    if not sort_only:
-                        j[k] = n[k]
+                        if not sort_only:
+                            j[k] = n[k]
+    except Exception as e:
+        print("Error after parsing line " + str(linenum) + ":")
+        print(line)
+        raise e from None
 
     with open(path + "de.json", "w", encoding="utf-8") as out:
         json.dump(j, out, sort_keys=True, indent=0, ensure_ascii=False)
